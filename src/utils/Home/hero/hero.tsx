431fdea3864dca955e93218c491679c82e3e64fc
Mehrab-developer <110405427+Mehrab-developer@users.noreply.github.com>
2023-11-21 04:21:27 +0500

    landing page

--- src/utils/Home/hero/hero.tsx
+++ src/utils/Home/hero/hero.tsx
@@ -66,30 +66,56 @@ function Hero() {
         </div>
 
         <div className="bg-[#fff] text-center md:pt-20 lg:pb-[5rem]  pt-[2rem] pb-[2rem]">
-          <h1 className="text-[#1F2F49]  text-5xl font-semibold  ">The No.1 Trusted Dating Site</h1>
+          <h1 className="text-[#1F2F49]  text-5xl font-semibold  ">
+            The No.1 Trusted Dating Site
+          </h1>
           <div className="grid lg:grid-cols-4   md:grid-cols-2 grid-cols-1    ">
             {datingSiteFeatures.map((feature, index) => (
               <div key={index} className=" mt-14">
-                <h2 className="text-[#1F2F49] font-semibold">{feature.title}</h2>
-                <p className="mt-3 lg:ps-10 lg:pe-10 text-[#676770]">{feature.description}</p>
+                <h2 className="text-[#1F2F49] font-semibold">
+                  {feature.title}
+                </h2>
+                <p className="mt-3 lg:ps-10 lg:pe-10 text-[#676770]">
+                  {feature.description}
+                </p>
               </div>
             ))}
           </div>
         </div>
       </div>
 
-     <div className="mx-auto relative">
-          <div className="md:grid-cols-6  grid-cols-1">
+      <div className="mx-auto relative">
+        <div className="grid md:grid-cols-2  grid-cols-1">
           <Image
             src="/Video-Banner.jpg"
             alt="My Image"
             width={750}
             height={750}
           />
-
+          <div className="text-center / justify-center flex flex-col">
+            <h1 className="text-[#1F2F49]  font-semibold text-3xl ">Explore Dating Advice</h1>
+            <p className="text-[#1F2F49] font-semibold mt-1">Be calm. Be kind. Be yourself.</p>
+            <p className="mt-6 ">
+              Hello, I’m Sarah and I’m the first Dating AI for Qiupid. What are
+              your preferences?
+            </p>
+           <div className="flex flex-col items-center"> 
+            <Button
+              children="Men"
+              css="bg-[#ED147D] pe-52 ps-52   pt-3 pb-3 text-[#fff] rounded-[30px] text-[13px] font-bold "
+            />
+            <Button
+              children="Men"
+              css="bg-[#ED147D] pe-52 ps-52 mt-5 mb-5   pt-3 pb-3 text-[#fff] rounded-[30px] text-[13px] font-bold "
+            />
+            <Button
+              children="Men"
+              css="bg-[#ED147D] pe-52 ps-52    pt-3 pb-3 text-[#fff] rounded-[30px] text-[13px] font-bold "
+            />
+            </div>  
           </div>
-     </div>
-
+        </div>
+      </div>
     </>
   );
 }
