05e7ae349cf29ede12efba61c8d3adb08d4d0acc
bilaljoyia <joyiab107@gmail.com>
2023-12-15 15:44:43 +0500

    signup

--- src/components/signUp/Signup.tsx
+++ src/components/signUp/Signup.tsx
@@ -186,7 +186,7 @@ const SignUp = (props: Props) => {
                     <ErrorMessage name="userName" component="div" className="text-red-500" />
                   </div>
 
-
+          <div className="grid lg:grid-cols-1 md:grid-cols-2 gap-2">
                   <div className='form-group mb-3'>
                     <label className='mb-2'>Email</label>
                     <Field type="text" name="email" className="w-full border-b-2 outline-none border-gray-300 rounded p-2" placeholder="Enter your email"
@@ -205,6 +205,7 @@ const SignUp = (props: Props) => {
                         handleFormChange('confirmEmail', e.target.value);
                       }} />
                     <ErrorMessage name="confirmEmail" component="div" className="text-red-500" />
+                  </div>
                   </div>
 
                   <div className='form-group mb-3'>
@@ -266,7 +267,7 @@ const SignUp = (props: Props) => {
               )}
               {step === 2 && (
                 <>
-                  <div className="form-group mb-3">
+                  <div className="form-group xl:mb-3">
                     <label className="block mb-2">Where do you Live?</label>
                     <select
                       className="w-full border border-gray-300 rounded p-2"
@@ -291,6 +292,7 @@ const SignUp = (props: Props) => {
                     />
                   </div>
 
+                <div className="grid lg:grid-cols-2 gap-2">
                   <div className="form-group mb-3">
                     <label className="block mb-2">Reason of Registering</label>
                     <select className="w-full border border-gray-300 rounded p-2"
@@ -318,7 +320,9 @@ const SignUp = (props: Props) => {
                       ))}
                     </select>
                   </div>
-                  <div className='text-center mt-5'>
+                  </div>
+
+                  <div className='text-center xl:mt-5'>
                     <button type="submit" className="login-btn border border-gray-300 rounded-full active:scale-95 bg-[#fb1086] hover:bg-pink-700 text-white"
                     >Sign up</button>
                   </div>
@@ -331,23 +335,25 @@ const SignUp = (props: Props) => {
 
         {step === 2 && (
           <>
-            <div className="flex items-center mt-2">
+            <div className="flex items-center xl:mt-2">
               <hr className="flex-1 border-t border-black" />
               <span className="px-2">OR</span>
               <hr className="flex-1 border-t border-black" />
             </div>
+            <div className="grid md:grid-cols-2 gap-2">
             <div className='text-center mt-2'>
-              <button className="google-btn border border-gray-300 rounded-full" onClick={handleGoogleSignup}>
+              <button className="px-8 py-2 border border-gray-300 rounded-full" onClick={handleGoogleSignup}>
                 <i className="fa-brands fa-google me-1" style={{ color: ' #c61010' }}></i>
                 Sign up with Google</button>
             </div>
             <div className='text-center mt-2'>
-              <button className="facebook-btn border border-gray-300 rounded-full" onClick={handleFacebookSignup}>
+              <button className="px-5 py-2  border border-gray-300 rounded-full" onClick={handleFacebookSignup}>
                 <i className="fa-brands fa-facebook me-1" style={{ color: 'rgb(19 16 198)' }}></i>
                 Sign up with Facebook</button>
             </div>
+            </div>
             <div className='text-center mt-2'>
-              <button className="google-btn border border-gray-300 rounded-full" >
+              <button className="px-8 py-2 border border-gray-300 rounded-full" >
                 <i className="fa-brands fa-apple me-1" style={{ color: 'rgb(28 27 27)' }}></i>
                 Sign up with Apple ID</button>
             </div>
@@ -361,7 +367,7 @@ const SignUp = (props: Props) => {
     </div>
     <div className="h-[85vh] flex  items-center justify-end  flex-col bg-[#ffe8ea]  ">
     <h1 className="font-bold  lg:text-4xl text-5xl  xl:text-5xl  text-[#f46194] text-center md:-mb-14">Muslim Marriage online </h1>
-    <Image src={Marrage} className='rounded-md ' alt='marriage' />
+    <Image src={Marrage} className='rounded-md ' alt='marriage' width={500} height={500} />
       </div>
     {/* <div className="col-span-2  xsm:hidden lg:block rounded-l-xl bg-[#fb1086]"></div> */}
     </div> 
