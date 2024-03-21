bd0f3ccd59151dae0db3607174b813b37f6f2623
hassanshahzadse <hassanshahzadvs@gmail.com>
2024-03-22 00:35:54 +0500

    new pages

---
+++ src/app/aboutus/page.tsx
@@ -0,0 +1,10 @@
+import AboutUs from '@/components/aboutus/Aboutus'
+import React from 'react'
+
+export default function AboutUsPage() {
+  return (
+    <>
+      <AboutUs/>
+    </>
+  )
+}
---
+++ src/components/blog/blog.tsx
@@ -0,0 +1,19 @@
+
+export default function Blog(){
+    return(
+        <>
+              <div>
+        <div className="round p-20 bg-[#ED147D]">
+          <h1 className="text-center mb-24 text-white text-4xl font-bold">
+            Blog
+          </h1>
+        </div>
+        <div className="container mx-auto leading-7 text-[20px]">
+          <div className="page-1 shadow-lg p-20 rounded-md">
+                <div className="text-center text-6xl text-slate-600">No Blog Post yet</div>
+                </div>
+          </div>
+        </div>
+        </>
+    )
+}
