f15ad81c8ab732312a1636a59e89e8255ba008a2
hassanshahzadse <hassanshahzadvs@gmail.com>
2024-03-23 19:36:38 +0500

    added transition

--- src/components/subscription/subscription.tsx
+++ src/components/subscription/subscription.tsx
@@ -1,70 +1,70 @@
 "use client";
-
-import React from "react";
-import { useState } from "react";
-import Layout from "../mainLayout/layout";
+import React, { useState } from "react";
 import Image from "next/image";
 import Logo from "/public/icons/LOGO-soulmate.png";
 import Navbar from "../navbar/Navbar";
 
-
 const SubscriptionComponents = () => {
   const [activeButton, setActiveButton] = useState("Subscription");
+  const [transitionDirection, setTransitionDirection] = useState("");
+  const [expandedIndex, setExpandedIndex] = useState(null);
 
-  const handleClick = () => {
-    setActiveButton(activeButton === "Subscription" ? "Boost" : "Subscription");
+  const handleToggle = (button:any) => {
+    setTransitionDirection(activeButton === "Subscription" ? "left" : "right");
+    setActiveButton(button);
+    setExpandedIndex(null)
   };
 
-  
   return (
     <>
-      <Navbar/>
-     <div className="mx-auto max-w-7xl  ps-2 pe-4 sm:px-6 lg:px-8 mt-[8vh] lg:mt-[15vh] ">
-      <div className=" flex lg:flex-row flex-col  lg:justify-between  justify-center  items-center  my-10">
-        <div className="flex flex-col items-center mt-14   lg:-mt-16">
-          <Image src={Logo} alt="Logo" width={100} height={100} />
-          <h3 className=" font-bold -mt-10  font-sm">Muslim Marriage Online</h3>
+    <Navbar />
+      <div className="mx-auto max-w-7xl  ps-2 pe-4 sm:px-6 lg:px-8 mt-[8vh] lg:mt-[15vh] ">
+        <div className=" flex lg:flex-row flex-col  lg:justify-between  justify-center  items-center  my-10">
+          <div className="flex flex-col items-center mt-14   lg:-mt-16">
+            <Image src={Logo} alt="Logo" width={100} height={100} />
+            <h3 className=" font-bold -mt-10  font-sm">
+              Muslim Marriage Online
+            </h3>
+          </div>
+          <div className="heading text-center space-y-4 mt-6 lg:mt-0">
+            <h1 className="text-4xl font-bold mb-0">Find Your Soulmate</h1>
+            <span className="text-3xl font-semibold mt-0">
+              {activeButton === "Subscription" ? "Subscription" : "Boost"} Plan
+            </span>
+          </div>
+          <div className="btn  mt-6 lg:mt-0">
+            <ToggleButton
+              activeButton={activeButton}
+              handleToggle={handleToggle}
+            />
+          </div>
         </div>
-        <div className="heading text-center space-y-4 mt-6 lg:mt-0">
-          <h1 className="text-4xl font-bold mb-0">Find Your Soulmate</h1>
-          <span className="text-3xl font-semibold mt-0">Subscription Plan</span>
-        </div>
-        <div className="btn  mt-6 lg:mt-0">
-          <ToggleButton
+
+        <div className="mt-14">
+          <CardsContainer
             activeButton={activeButton}
-            setActiveButton={setActiveButton}
+            transitionDirection={transitionDirection}
+            setExpandedIndex={setExpandedIndex}
+            expandedIndex={expandedIndex}
+          />
+        </div>
+
+        <div className="bg-[#FD307A] h-[5vh] fixed left-0 w-full bottom-0 z-10 ">
+          <Image
+            src="/Navbar/NavbarThin1.png"
+            alt="Description"
+            className="rounded-lg"
+            objectFit="cover"
+            fill
           />
         </div>
       </div>
-
-     <div className="mt-14">
-        <CardsContainer activeButton={activeButton} />
-      </div> 
-
-      <div className="bg-[#FD307A] h-[5vh] fixed left-0 w-full bottom-0 z-10 ">
-      <Image
-              src="/Navbar/NavbarThin1.png"
-              alt="Description"
-              className="rounded-lg"
-              objectFit="cover"
-              fill
-            />
-      </div>
-      </div> 
     </>
   );
 };
-
 export default SubscriptionComponents;
 
-
-
-const ToggleButton: React.FC<any> = ({ setActiveButton, activeButton }) => {
-  
-  const handleClick = (button:any) => {
-    setActiveButton(button);
-  };
-
+const ToggleButton: React.FC<any> = ({ activeButton,handleToggle }) => {
   return (
     <div className="border space-x-1 rounded-full">
       <button
@@ -73,7 +73,7 @@ const ToggleButton: React.FC<any> = ({ setActiveButton, activeButton }) => {
             ? "bg-[#FD307A] text-white"
             : "bg-transparent text-black"
         }`}
-        onClick={() => handleClick("Subscription")}
+        onClick={() => handleToggle("Subscription")}
       >
         Subscription
       </button>
@@ -83,7 +83,7 @@ const ToggleButton: React.FC<any> = ({ setActiveButton, activeButton }) => {
             ? "bg-[#FD307A] text-white"
             : "bg-transparent text-black"
         }`}
-        onClick={() => handleClick("Boost")}
+        onClick={() => handleToggle("Boost")}
       >
         Boost
       </button>
@@ -91,81 +91,91 @@ const ToggleButton: React.FC<any> = ({ setActiveButton, activeButton }) => {
   );
 };
 
-const Card: React.FC<any> = ({ title, days, price }) => {
-  const [isExpanded, setIsExpanded] = useState<boolean>(false);
-
-  const expandCard = () => {
-    setIsExpanded(!isExpanded);
-  };
-  return (
-    <div className="p-4">
-    <div className={`bg-[#ffffff] p-6  ${isExpanded ? 'scale-125' : ''}    m-2  rounded-3xl  border-[#707070]  drop-shadow-3xl border-2 `}
-      onClick={expandCard}
-    >
-      <h3 className="text-2xl font-bold text-center">{title}</h3>
-      <p className="text-gray-600 text-center py-3">{days}</p>
-      <div className="mt-16 text-center flex flex-col ">
-        <span className="py-3">{price}</span>
-        <button className={`border-2 border-gray-500 px-5    ${isExpanded ? 'bg-[#FE035C] text-[#ffffff]' : 'bg-[#ffffff] '}          rounded-md mx-auto`}>
-          Buy now
-        </button>
+  const Card: React.FC<any> = ({ title, days, price, isExpanded, expandCard }) => {
+    return (
+      <div className={`p-4 ${isExpanded ? "scale-125" : ""}`}>
+        <div
+          className={`bg-[#ffffff] py-6 px-1 ${isExpanded ? "expanded" : ""}    m-2  rounded-3xl  border-[#808080]  drop-shadow-3xl `}
+          onClick={expandCard}
+          style={{ boxShadow: "5px 5px 5px gray", borderWidth: "1px" }}
+        >
+          <h3 className={`${isExpanded ? "text-4xl" : "text-3xl"} font-bold text-center`}>{title}</h3>
+          <p className="text-gray-600 text-center py-3">{days}</p>
+          <div className="mt-16 text-center flex flex-col ">
+            <span className="py-3">{price}</span>
+            <button
+              className={`border-2 border-gray-500 px-5    ${
+                isExpanded
+                  ? "bg-[#FE035C] text-[#ffffff]"
+                  : "bg-[#ffffff] "
+              }          rounded-md mx-auto`}
+            >
+              Buy now
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-  );
-};
-
-const CardsContainer: React.FC<any> = ({ activeButton }) => {
-  const subscriptionCards = [
-    {
-      title: "Silver",
-      days: "5 day plan",
-      price: "10$",
-    },
-    {
-      title: "Gold",
-      days: "1 month plan",
-      price: "20$",
-    },
-    {
-      title: "Diamond",
-      days: "3 month plan",
-      price: "40$",
-    },
-    {
-      title: "Platinum",
-      days: "6 month plan",
-      price: "100$",
-    },
-  ];
-  const bootsCards = [
-    {
-      title: "1 week boost",
-    },
-    {
-      title: "3 weeks boost",
-    },
-    {
-      title: "1 month boost",
-    },
-    {
-      title: "2 month boost",
-    },
-  ];
-
-  const cards =
-    activeButton === "Subscription" ? subscriptionCards : bootsCards;
-
-  return (
-    <div className=" grid lg:grid-cols-4 md:grid-cols-2 ">
-      {cards.map((data: any, index) => (
-        <Card
-          key={index}
-          title={data.title}
-          days={data.days}
-          price={data.price}
-        />
-      ))}
-    </div>
-  );
-};
+    );
+  };
+  
+  const CardsContainer: React.FC<any> = ({ activeButton, transitionDirection,expandedIndex,setExpandedIndex }) => {
+  
+    const handleExpand = (index:any) => {
+      setExpandedIndex(index === expandedIndex ? null : index);
+    };
+  
+    const subscriptionCards = [
+      {
+        title: "Silver",
+        days: "5 day plan",
+        price: "10$",
+      },
+      {
+        title: "Gold",
+        days: "1 month plan",
+        price: "20$",
+      },
+      {
+        title: "Diamond",
+        days: "3 month plan",
+        price: "40$",
+      },
+      {
+        title: "Platinum",
+        days: "6 month plan",
+        price: "100$",
+      },
+    ];
+  
+    const boostCards = [
+      {
+        title: "1 week boost",
+      },
+      {
+        title: "3 weeks boost",
+      },
+      {
+        title: "1 month boost",
+      },
+      {
+        title: "2 month boost",
+      },
+    ];
+  
+    const cards:any = activeButton === "Subscription" ? subscriptionCards : boostCards;
+  
+    return (
+      <div className={`grid lg:grid-cols-4 md:grid-cols-2 transition-all duration-500 ${transitionDirection === 'left' ? 'transition-left-sub' : 'transition-right-sub'}`}>
+        {cards.map((data:any, index:any) => (
+          <Card
+            key={index}
+            title={data.title}
+            days={data.days}
+            price={data.price}
+            isExpanded={expandedIndex === index}
+            expandCard={() => handleExpand(index)}
+          />
+        ))}
+      </div>
+    );
+  };
