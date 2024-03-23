40f844fd158eac8719c757b36a6dd4af5c4dbf52
hassanshahzadse <hassanshahzadvs@gmail.com>
2024-03-23 15:53:40 +0500

    n

--- src/components/aboutus/Aboutus.tsx
+++ src/components/aboutus/Aboutus.tsx
@@ -1,54 +1,235 @@
+import React from "react";
+import "./privacy.css";
 
-export default function AboutUs(){
-    return(
-        <>
-             <section className="bg-[#ED147D]">
-        <div className="container mx-auto p-6 h-[10vh]  items-center">
-          <h1 className="text-3xl text-white font-bold">About Us</h1>
+export default function AboutUs() {
+  return (
+    <>
+      <div>
+        <div className="round p-20 bg-[#ED147D]">
+          <h1 className="text-center mb-24 text-white text-4xl font-bold">
+            Privacy & Policy
+          </h1>
         </div>
-      </section>
-      <section className="container mx-auto shadow-md p-7 h-[85vh]">
-        <div className="pt-20">
-          <p>
-            Welcome to Muslim Marriage Online, where love meets tradition in the
-            digital age. We are dedicated to fostering meaningful connections
-            within the Muslim community, rooted in faith, respect, and
-            understanding. Our platform provides a safe and trusted space for
-            individuals seeking marriage to connect with like-minded partners
-            who share their values and beliefs.
-          </p>
-          <p className="mt-3">
-            At Muslim Marriage Online, we understand the importance of marriage
-            in Islam and the profound impact it has on individuals, families,
-            and communities. Guided by Islamic principles, we strive to
-            facilitate halal and fulfilling relationships that lead to lifelong
-            companionship and mutual growth.
-          </p>
-          <p className="mt-3">
-            Our platform is designed to cater to the diverse needs of Muslims
-            worldwide, whether you're searching for a partner in your local
-            community or across the globe. With advanced search filters and
-            personalized matchmaking services, we make it easier for you to find
-            your ideal match while honoring your cultural and religious
-            preferences.
-          </p>
-          <p className="mt-3">
-            Transparency and privacy are paramount to us. We ensure that your
-            personal information remains secure and confidential throughout your
-            journey on our platform. Our dedicated team works tirelessly to
-            maintain the integrity of our community and uphold the values of
-            respect, decency, and honesty.
-          </p>
-          <p className="mt-3">
-            Whether you're a devout Muslim looking to start a family or someone
-            interested in embracing Islamic values in your search for love,
-            Muslim Marriage Online is here to support you every step of the way.
-            Join us today and embark on a journey towards a fulfilling and
-            blessed marriage, guided by faith and love.
-          </p>
+        <div className="container mx-auto leading-7 text-[20px]">
+          <div className="page-1 shadow-lg p-20 rounded-md">
+            <h1 className="text-3xl font-semibold mb-6">Privacy Policy</h1>
+            <p>
+              Our pledge to protect your privacy is exemplified in our Privacy
+              Policy, which was created to reflect our dedication to providing
+              exceptional products and services, while maintaining complete
+              transparency and honesty with our clients and partners. This
+              policy aims to explain how we gather, use, and protect the
+              personal data that you share with us, thus helping you make
+              informed decisions about using our website, products, and
+              services. The policy is continually revised alongside evolving
+              technologies, industry practices, and customer requirements. We
+              collect two kinds of information during your visit to our website:
+              <p className="my-6">
+                <span className="font-bold">1)</span> Personal data that you
+                intentionally share with us, collected on an individualistic
+                basis.
+              </p>
+              <p className="my-6">
+                <span className="font-bold">2)</span> Information related to how
+                our website is used, aggregated from your and other users'
+                browsing activities.
+              </p>
+              <p>
+                <span className="font-bold">1)</span> Personal Information that
+                you voluntarily provide: Credit Card Information. If you decide
+                to buy our products or services or those of our partners, you
+                may have to provide personal details and approval to access
+                information from numerous credit services. You might have to
+                share the following data:
+              </p>
+              <div className="my-9">
+                <p>· Full Name</p>
+                <p>· Postal Address</p>
+                <p>· Email Address</p>
+                <p>· Credit Card Details</p>
+                <p>· Residential and Business Phone numbers</p>
+                <p>
+                  · Additional personal data (e.g., mother's maiden name) and
+                  Email Details Apart from sharing the above data with our
+                  associates, if you decide to continue the conversation with us
+                  via email, we may store the context of your emails along with
+                  your email ID and our responses. We employ the same level of
+                  safeguarding your electronic communications as we do for
+                  information received through traditional means like mail or
+                  phone calls.
+                </p>
+              </div>
+              <span className="font-bold">2. </span> Website Usage Info Just
+              like other business websites, our website uses a common technology
+              known as "cookies" (explained later in "What Are Cookies?") and
+              web server logs to gather data about how our website is utilized.
+              This might include the time and date of visits, pages seen, the
+              amount of time spent on our site, and the websites visited before
+              and after visiting our website. This data is gathered collectively
+              and is not linked to you as an individual.
+            </p>
+          </div>
+          {/* ****** Page 2 ****** */}
+          <div className="page-2 shadow-lg p-20 my-10 rounded-md">
+            <h1 className="text-2xl font-semibold mb-6">
+              What is the Purpose of the Information We Collect?
+            </h1>
+            <p>
+              <p className="my-8">
+                On a broad level, we use personal information to manage our
+                business operations, offer customer support, and avail other
+                products and services to our existing and potential customers.
+              </p>
+              <p>
+                From time to time, we might use the information gathered to
+                notify you about major updates to our website, new services, and
+                special promotions we think might interest you. The lists
+                utilized to send you promotional offers are created and
+                maintained keeping in mind our conventional corporate norms that
+                are aimed at ensuring the security and privacy of our customers'
+                personal data.
+              </p>
+              <p className="my-8">
+                Our website provides you with the means to inform us if you
+                prefer not to receive these offers. Muslim Marriage Online
+                employs Google AdWords remarketing lists for remarketing
+                purposes. As an independent third party, Google.com gathers
+                details pertaining to your website visits and projects fitting
+                ads on their websites as well as their network. Muslim Marriage
+                Online does not amass this data. This is a system based on
+                cookies. Most internet browsers enable the deletion of cookies.
+                Visitors have the option to decline Google's cookie usage by
+                going to the Ads Preferences Manager. Alternatively, you can
+                choose to opt-out by visiting the Network Advertising Initiative
+                opt- out page.
+              </p>
+              <h1 className="text-2xl font-semibold mt-14 mb-8">
+                What is the Concept of Cookies?
+              </h1>
+              Cookies are elements present in web browser software that enable
+              web servers to identify the specific computer used to visit a
+              particular website. They are minor bits of information saved on
+              the user's computer by their web browser. The main purpose of
+              cookies is to keep a tab on the data a user viewed on a single web
+              page, which subsequently simplifies user interactions with the
+              website going forward. Furthermore, cookies expedite the user's
+              activities on linked web pages, enabling smooth navigation from
+              one page to another, as well as successful completion of online
+              transactions. Cookies are designed to enhance your online presence
+              and personalize your online experience.
+              <h1 className="text-2xl font-semibold mt-14 mb-8">
+                How is User Information Collected from Cookies Utilized?
+              </h1>
+              We deploy browser software tools such as cookies, along with web
+              server logs, to acquire key details about our website users’
+              browsing patterns, which helps us to perpetually refine our
+              website and cater efficiently to our customers. The acquired
+              information aids us in designing and structuring our web pages to
+              be as user-friendly as possible. We also regularly update our
+              website in accordance with the needs and preferences of our
+              customers and potential patrons. Additionally, cookies provide us
+              with essential business and technical insights. Information stored
+              in cookies enables us to track the routes followed by users
+              arriving at our website, from one page to another. Web server logs
+              help us in keeping count of the number of individuals visiting our
+              website and assess our website's visitor capacity. We refrain from
+              using these technologies to capture personal details like email
+              addresses. However, we do use them to target online banner
+              advertisements or similar responses.
+            </p>
+          </div>
+
+          <div className="page-3 shadow-lg p-20 my-10 rounded-md">
+            <h1 className="text-2xl font-semibold mt-14 mb-8">
+              Dispensing Information to Associated Entities
+            </h1>
+            <p>
+              On occasion, you may see promotions from external companies on our
+              website. We strive to choose product or service providers that are
+              reliable and offer privacy protections to their patrons.
+              Nevertheless, we can't provide assurances about these companies'
+              practices and policies.
+              <h1 className="text-2xl font-semibold mt-14 mb-8">
+                Disclosing Information to Strategic Allies{" "}
+              </h1>
+              Partnerships or marketing alliances with third parties may occur,
+              allowing them access to personal data such as your name, address,
+              phone number, and email. This is to provide you with details about
+              products and services that may pique your interest. In such
+              partnerships or alliances, we maintain complete ownership rights
+              to the information and will never share data concerning your
+              social security number or private financial information.
+              <h1 className="text-2xl font-semibold mt-14 mb-8">
+                Announcement of New Offerings and Modifications{" "}
+              </h1>
+              From time to time, we may use collected data to inform you about
+              significant website updates, new services, and special promotions
+              that we believe you'll find beneficial. As our client, we offer
+              you the choice of opting out of these offers. This can be done by
+              utilizing the response box provided when you receive such an offer
+              or by starting a support ticket at our member's support section.
+              <h1 className="text-xl font-semibold mt-14 mb-8">
+                How Do We Maintain the Safety of Information Transfers?{" "}
+              </h1>
+              Stripe Inc. is our chosen payment platform. We do not collect any
+              information about your credit card except that which Stripe Inc.
+              provides us with when you successfully use your card on our
+              website. It's important to note that any emails you send to us may
+              not be secure unless we let you know that there will be safety
+              measures in place beforehand. Therefore, we advise against sharing
+              confidential details such as social security or account numbers
+              through an insecure email.
+              <h1 className="text-2xl font-semibold mt-14 mb-8">
+                How Do We Safeguard Your Data?
+              </h1>
+              We employ encryption/security software to ensure the privacy of
+              the personal information we gather and to protect it from
+              unauthorized access, accidental alteration, loss, or disclosure.
+              We regularly review our operations and business practices to
+              ensure that they align with company-wide policies and procedures
+              that preserve the security, confidentiality, and integrity of our
+              data. We uphold stringent corporate values, ethical guidelines,
+              policies, and practices dedicated to protecting customer data.
+              Typically, our business operations restrict employee access to
+              sensitive information, limiting its use and sharing only to
+              authorized individuals and approved processes.
+            </p>
+          </div>
+          <div className="page-2 shadow-lg p-20 my-10 rounded-md">
+            <p>
+              <h1 className="text-2xl font-semibold mt-14 mb-8">
+                How Can You Access and Amend Your Data?
+              </h1>
+              You can access all personal identifiable data we have collected
+              from you online and stored in our database by submitting a support
+              ticket at our members support portal.
+              <h1 className="text-2xl font-semibold mt-14 mb-8">
+                Do We Share Information with External Entities?
+              </h1>
+              We may disclose aggregated data about our customers, sales,
+              website traffic trends, and other website-related data to our
+              partners or trustworthy third parties. However, this data will not
+              include personally identifiable information, except as outlined in
+              this Privacy Policy.
+              <h1 className="text-2xl font-semibold mt-14 mb-8">
+                What if we are legally forced to revealinformation?
+              </h1>
+              In circumstances where we are forced by law to disclose
+              information, or in situations where we genuinely believe that it
+              is required by law or for the safeguarding of our legal rights, we
+              will do so.
+              <h1 className="text-2xl font-semibold mt-14 mb-8">
+                What about other websites that are connected to ours?
+              </h1>
+              We hold no liability for the policies implemented by websites that
+              are linked to or from our site, nor the information or content
+              that they contain. Many times, links to other websites are
+              provided purely as references to information that may be
+              beneficial to those using our website.
+            </p>
+          </div>
         </div>
-      </section>
-      <div className="h-[5vh] bg-[#ED147D] "></div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
